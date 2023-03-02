a0fc73d64e8f651ebf837c7d98e3e6e0a0593328
Santiago Pagge <santy.pagge@gmail.com>
2023-03-02 19:59:29 -0300

    Vuelvo a minusculas el diets del model

--- PI-Food-main/api/src/models/Diets.js
+++ PI-Food-main/api/src/models/Diets.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define(
-        'Diets',
+        'diets',
         {
             name: {
                 type: DataTypes.STRING,
