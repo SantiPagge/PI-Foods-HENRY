bcb27a6115502510c5cd333b6e434f425a830dd4
Santiago Pagge <santy.pagge@gmail.com>
2023-03-03 20:25:09 -0300

    a ver ahora

--- PI-Food-main/client/src/App.js
+++ PI-Food-main/client/src/App.js
@@ -7,7 +7,7 @@ import { Home } from './Views/Home/Home'
 import { Detail } from './Views/Details/Detail'
 import { RecipesForm } from './Views/RecipesForm/RecipesForm';
 
-axios.defaults.baseURL = "pi-foods-henry-production-ec7e.up.railway.app/";
+axios.defaults.baseURL = "pi-foods-henry-production-ec7e.up.railway.app";
 // axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
