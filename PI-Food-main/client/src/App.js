2680d038a7e7a170a764f32e4189663d5cff930f
Santiago Pagge <santy.pagge@gmail.com>
2023-08-30 20:36:20 -0300

    Cambiando a sintaxis ReactV6

--- PI-Food-main/client/src/App.js
+++ PI-Food-main/client/src/App.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React from 'react';
 import style from './App.css'
-import { Routes, Route } from 'react-router';
+//import { Routes, Route } from 'react-router';
+import { useRoutes, Outlet, Route } from 'react-router-dom'; // Cambiado a useRoutes y Outlet
 import { Welcome } from './Views/Welcome/Welcome';
 import { Home } from './Views/Home/Home'
 import { Detail } from './Views/Details/Detail'
@@ -11,14 +12,16 @@ axios.defaults.baseURL = "https://pi-foods-henry-production-053d.up.railway.app"
 // axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
+  const routes = useRoutes([ // Cambiado a useRoutes
+    { path: '/', element: <Welcome /> },
+    { path: '/home', element: <Home /> },
+    { path: '/create', element: <RecipesForm /> },
+    { path: '/detail/:id', element: <Detail /> }
+  ]);
+
   return (
     <div className={style.all}>
-        <Routes>
-          <Route exact path='/' component={Welcome}/>
-          <Route exact path='/home' component={Home}/>
-          <Route path='/create' component={RecipesForm}/>
-          <Route path='/detail/:id' component={Detail}/>
-        </Routes>
+      {routes}
     </div>
   );
 }
